bc278b87c1eeab57b0e29f610f1afb8e86070de9
Jeff Lindsay <progrium@gmail.com>
2013-06-08 03:48:13 -0700

    comment bootstrap sections, fix hostname variable, a readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dokku
+
+Docker powered mini-Heroku. The smallest PaaS implementation you've ever seen.
+
+## Requirements
+
+Assumes Ubuntu 13 right now. Ideally name the host so it gets/has a hostname of a domain you have pointing to it.
+
+## Installing
+
+    $ wget -qO- j.mp/dokku-bootstrap | bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+# Docker and base dependencies
 apt-get install -y linux-image-extra-`uname -r`
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:dotcloud/lxc-docker
@@ -5,28 +6,29 @@ apt-get update
 apt-get install -y lxc-docker
 apt-get install -y git ruby nginx make
 
+# gitreceive
 cd /usr/local/bin
 wget https://raw.github.com/progrium/gitreceive/master/gitreceive
 chmod +x gitreceive
 gitreceive init
 
+# buildstep
 cd ~
 git clone https://github.com/progrium/buildstep.git
 cd buildstep
 cp buildstep /home/git/buildstep
 make
 
-
+# dokku (this!)
 cd ~
 git clone https://github.com/progrium/dokku.git
 cd dokku
 cp receiver /home/git/receiver
 cp nginx-app-conf /home/git/nginx-app-conf
 cp nginx-reloader.conf /etc/init/nginx-reloader.conf
-
+stop nginx-reloader
 start nginx-reloader
-
 echo $HOSTNAME > /home/git/DOMAIN
 
 echo "Be sure to upload a public key for your user:"
-echo "  cat ~/.ssh/id_rsa.pub | ssh root@$HOStNAME \"gitreceive upload-key progrium\""
+echo "  cat ~/.ssh/id_rsa.pub | ssh root@$HOSTNAME \"gitreceive upload-key progrium\""
