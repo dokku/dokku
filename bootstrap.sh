afc567fb91355e9989afe1174c774118e4d9d6f2
Jeff Lindsay <progrium@gmail.com>
2013-06-09 18:13:38 -0700

    install deploystep

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,7 @@ cd ~
 git clone ${DOKKU_REPO}
 cd dokku
 cp receiver /home/git/receiver
+cp deploystep /home/git/deploystep
 cp nginx-app-conf /home/git/nginx-app-conf
 cp nginx-reloader.conf /etc/init/nginx-reloader.conf
 stop nginx-reloader
