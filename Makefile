ad2fd7fb557ab92021b6b959f8042870dee81935
Jeff Lindsay <progrium@gmail.com>
2013-06-11 20:16:07 -0700

    moving gitreceiv into makefile with sshcommand

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
+GITRECEIVE_URL = https://raw.github.com/progrium/gitreceive/master/gitreceive
+SSHCOMMAND_URL = https://raw.github.com/progrium/sshcommand/master/sshcommand
 
 all: install
 
-install: submodule
+install: submodule gitreceive sshcommand
 	cp receiver /home/git/receiver
 	cp deploystep /home/git/deploystep
 	cp buildstep/buildstep /home/git/buildstep
@@ -11,6 +13,15 @@ install: submodule
 submodule:
 	git submodule init
 	git submodule update
+
+gitreceive:
+	wget -O /usr/local/bin/gitreceive ${GITRECEIVE_URL}
+	chmod +x /usr/local/bin/gitreceive
+	[[ ! -d /home/git ]] && gitreceive init
+
+sshcommand:
+	wget -O /usr/local/bin/sshcommand ${SSHCOMMAND_URL}
+	chmod +x /usr/local/bin/sshcommand
 
 count:
 	cat receiver deploystep bootstrap.sh nginx-app-conf nginx-reloader.conf | wc -l
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,8 @@
-# Repository locations - set environment variables to override defaults
-#  e.g. DOKKU_REPO=https://github.com/yourusername/dokku.git bootstrap.sh
-GITRECEIVE_URL=${GITRECEIVE_URL:-"https://raw.github.com/progrium/gitreceive/master/gitreceive"}
 DOKKU_REPO=${DOKKU_REPO:-"https://github.com/progrium/dokku.git"}
 
 apt-get install -y linux-image-extra-`uname -r` software-properties-common
 add-apt-repository -y ppa:dotcloud/lxc-docker
 apt-get update && apt-get install -y lxc-docker git ruby nginx make
-
-wget -O /usr/local/bin/gitreceive ${GITRECEIVE_URL}
-chmod +x /usr/local/bin/gitreceive
-gitreceive init
 
 cd ~ && git clone ${DOKKU_REPO}
 cd dokku && make install
@@ -19,11 +12,8 @@ start nginx-reloader
 /etc/init.d/nginx start
 
 echo "include /home/git/*/nginx.conf;" > /etc/nginx/conf.d/dokku.conf
-if [[ $(dig +short $HOSTNAME) ]]; then
-  echo $HOSTNAME > /home/git/DOMAIN
-else
-  echo $HOSTNAME > /home/git/HOSTNAME
-fi
+[[ $(dig +short $HOSTNAME) ]] && HOSTFILE=DOMAIN || HOSTFILE=HOSTNAME
+echo $HOSTNAME > /home/git/$HOSTFILE
 
 echo
 echo "Be sure to upload a public key for your user:"
