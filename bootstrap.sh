83c3edd4af4fb8d50d2bf6376d015b338f91eb18
Lukas Schauer <lukas2511@xxpro.net>
2013-11-27 00:53:31 +0100

    Update bootstrap.sh
    added missing dependency (python-software-properties for add-apt-repository)

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ then
 fi
 
 apt-get update
-apt-get install -y git make curl software-properties-common
+apt-get install -y git make curl software-properties-common python-software-properties
 
 cd ~ && test -d dokku || git clone $DOKKU_REPO
 cd dokku
