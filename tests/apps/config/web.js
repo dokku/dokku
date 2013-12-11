48b4e330a3ec3fe4f05f2f53ed721a56b0ddc31d
Paul Lietar <paul@lietar.net>
2013-12-11 21:41:16 +0000

    Add a config test.

---
+++ tests/apps/config/web.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(process.env.CONFTEST);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
