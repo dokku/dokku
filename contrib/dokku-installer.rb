69c13f1bd3f62e2817a74c2239c50c5295665c82
Lucas Wilson-Richter <da.maestro@gmail.com>
2014-03-12 07:47:21 +1100

    Improve IP detection regex so it isn't fooled by domains that look like IP addresses.

--- contrib/dokku-installer.rb
+++ contrib/dokku-installer.rb
@@ -104,7 +104,7 @@ __END__
 				});
 		}
 		function update() {
-			if ($("#vhost").is(":checked") && $("#hostname").val().match(/(\d{1,3}\.){3}\d{1,3}/)) {
+			if ($("#vhost").is(":checked") && $("#hostname").val().match(/^(\d{1,3}\.){3}\d{1,3}$/)) {
 				alert("In order to use virtualhost naming, the hostname must not be an IP but a valid domain name.")
 				$("#vhost").prop('checked', false);
 			}
