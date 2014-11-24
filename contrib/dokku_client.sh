2141435ce13b01fd87dda5ad960aaef0a3a11054
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-11-23 19:54:10 -0500

    Rename DOKKU_CONTROLLER to DOKKU_HOST

--- contrib/dokku_client.sh
+++ contrib/dokku_client.sh
@@ -1,4 +1,4 @@
-if [ -z "$DOKKU_CONTROLLER" ]
+if [ -z "$DOKKU_HOST" ]
 then
 	function dokku {
 		appname=$(git remote -v 2>/dev/null | grep dokku | head -n 1 | cut -f1 -d' ' | cut -f2 -d':' 2>/dev/null)
@@ -9,9 +9,9 @@ then
 		if [ "$1" = "create" ]
 		then
 			appname=$(echo "print(elfs.GenName())" | lua -l elfs)
-			if git remote add dokku dokku@$DOKKU_CONTROLLER:$appname
+			if git remote add dokku dokku@$DOKKU_HOST:$appname
 			then
-				echo "-----> Dokku remote added at $DOKKU_CONTROLLER"
+				echo "-----> Dokku remote added at $DOKKU_HOST"
 				echo "-----> Application name is $appname"
 			else
 				echo "!      Dokku remote not added! Do you already have a dokku remote?"
@@ -21,12 +21,12 @@ then
 		else
 			if [ -z "$donotshift" ]
 			then
-				ssh dokku@$DOKKU_CONTROLLER $*
+				ssh dokku@$DOKKU_HOST $*
 				exit
 			fi
 			verb=$1
 			shift
-			ssh dokku@$DOKKU_CONTROLLER "$verb" "$appname" $@
+			ssh dokku@$DOKKU_HOST "$verb" "$appname" $@
 		fi
 	}
 fi
