e04b90c416b0c760b990170bfa879dc8611b55b1
Leonardo Dino <leonardo@kunstdesign.com.br>
2014-12-23 22:31:29 -0300

    missed the advanced installation link
    it’s linked in the instalation page, but it’s broken.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ export DOKKU_REPO=${DOKKU_REPO:-"https://github.com/progrium/dokku.git"}
 
 if ! command -v apt-get &>/dev/null
 then
-  echo "This installation script requires apt-get. For manual installation instructions, consult https://github.com/progrium/dokku ."
+  echo "This installation script requires apt-get. For manual installation instructions, consult http://progrium.viewdocs.io/dokku/advanced-installation ."
   exit 1
 fi
 
