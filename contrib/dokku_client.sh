f293e9026d70fc8e335a14f2d8c218b6073892a6
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-01-11 22:36:12 -0500

    Better dokku-client bash formatting

--- contrib/dokku_client.sh
+++ contrib/dokku_client.sh
@@ -1,13 +1,11 @@
-if [ -z "$DOKKU_HOST" ]
-then
+if [[ ! -z $DOKKU_HOST ]]; then
 	function dokku {
 		appname=$(git remote -v 2>/dev/null | grep dokku | head -n 1 | cut -f1 -d' ' | cut -f2 -d':' 2>/dev/null)
-		if [ "$?" != "0" ]
-		then
+		if [[ "$?" != "0" ]]; then
 			donotshift="YES"
 		fi
-		if [ "$1" = "create" ]
-		then
+
+		if [[ "$1" = "create" ]]; then
 			appname=$(echo "print(elfs.GenName())" | lua -l elfs)
 			if git remote add dokku dokku@$DOKKU_HOST:$appname
 			then
@@ -18,15 +16,16 @@ then
 				return
 			fi
 			git push dokku master
-		else
-			if [ -z "$donotshift" ]
-			then
-				ssh dokku@$DOKKU_HOST $*
-				exit
-			fi
-			verb=$1
-			shift
-			ssh dokku@$DOKKU_HOST "$verb" "$appname" $@
+      return $?
 		fi
+
+		if [[ -z "$donotshift" ]]; then
+			ssh dokku@$DOKKU_HOST $*
+			exit
+		fi
+
+		verb=$1
+		shift
+		ssh dokku@$DOKKU_HOST "$verb" "$appname" $@
 	}
 fi
