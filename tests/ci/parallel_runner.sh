27b79222c146daf32d7e8ab43a54f91a23717f13
Michael Hobbs <michaelshanehobbs@gmail.com>
2015-07-08 09:29:50 -0700

    catch test setup errors

--- tests/ci/parallel_runner.sh
+++ tests/ci/parallel_runner.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-set -eo pipefail; set -x
+set -eo pipefail
 
 MODE="$1"; MODE=${MODE:="testing"}
 
 setup_circle() {
+  echo "=====> setup_circle on CIRCLE_NODE_INDEX: $CIRCLE_NODE_INDEX"
   sudo -E CI=true make -e sshcommand
   # need to add the dokku user to the docker group
   sudo usermod -G docker dokku
@@ -19,28 +20,37 @@ setup_circle() {
   make -e ci-dependencies
 }
 
+if [[ -n "$CIRCLE_NODE_INDEX" ]] && [[ "$MODE" == "setup" ]]; then
+  case "$CIRCLE_NODE_INDEX" in
+      3)
+        setup_circle buildstack
+        exit $?
+        ;;
+      *)
+        setup_circle
+        exit $?
+        ;;
+  esac
+fi
+
 case "$CIRCLE_NODE_INDEX" in
   0)
     echo "=====> make unit-tests (1/2) on CIRCLE_NODE_INDEX: $CIRCLE_NODE_INDEX"
-    [[ "$MODE" == "setup" ]] && (setup_circle ; exit $?)
     sudo -E UNIT_TEST_BATCH=1 make -e unit-tests
     ;;
 
   1)
     echo "=====> make unit-tests (2/2) on CIRCLE_NODE_INDEX: $CIRCLE_NODE_INDEX"
-    [[ "$MODE" == "setup" ]] && (setup_circle ; exit $?)
     sudo -E UNIT_TEST_BATCH=2 make -e unit-tests
     ;;
 
   2)
     echo "=====> make deploy-tests (herokuish release) on CIRCLE_NODE_INDEX: $CIRCLE_NODE_INDEX"
-    [[ "$MODE" == "setup" ]] && (setup_circle ; exit $?)
     sudo -E make -e deploy-tests
     ;;
 
   3)
     echo "=====> make deploy-tests (herokuish master) on CIRCLE_NODE_INDEX: $CIRCLE_NODE_INDEX"
-    [[ "$MODE" == "setup" ]] && (setup_circle buildstack ; exit $?)
     sudo -E make -e deploy-tests
     ;;
 esac
