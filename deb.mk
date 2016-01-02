63d961afa0926fe63bd9fb36823aab7dd02d380b
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-01-01 23:57:18 -0500

    Revert "Fix lintian warning: file-in-unusual-dir"
    This reverts commit 3fb59fd04d835aed77cb30df1227409b77fff1b1.
    
    Changing the path to dokku cannot be done in a patch release, as it
    would break existing sshcommand entries.

--- deb.mk
+++ deb.mk
@@ -83,13 +83,13 @@ deb-dokku: deb-setup
 	mkdir -p /tmp/tmp /tmp/build
 
 	cp -r debian /tmp/build/DEBIAN
-	mkdir -p /tmp/build/usr/bin
+	mkdir -p /tmp/build/usr/local/bin
 	mkdir -p /tmp/build/var/lib/dokku/core-plugins/available
 	mkdir -p /tmp/build/usr/share/man/man1
 	mkdir -p /tmp/build/usr/share/dokku/contrib
 	mkdir -p /tmp/build/usr/share/doc/dokku
 
-	cp dokku /tmp/build/usr/bin
+	cp dokku /tmp/build/usr/local/bin
 	cp LICENSE /tmp/build/usr/share/doc/dokku/copyright
 	cp -r plugins/* /tmp/build/var/lib/dokku/core-plugins/available
 	find plugins/ -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | while read plugin; do touch /tmp/build/var/lib/dokku/core-plugins/available/$$plugin/.core; done
