7a9cf8abb11e00382a134a8bef182c0fd4632854
Morris Jobke <hey@morrisjobke.de>
2016-02-15 14:59:14 +0100

    Adds support for ArchLinux as host OS
    * fix rsyslog handling
    * don't hardcode dokku path
    * add documentation for ArchLinux package build
    * add vagrant machine to run the ArchLinux specific steps
    * add step in release process for ArchLinux
    * enhance docs for easy ArchLinux steps

---
+++ arch.mk
@@ -0,0 +1,28 @@
+.PHONY: arch-all arch-dokku
+
+arch-all: arch-dokku
+	echo "Done"
+
+arch-setup:
+	echo "-> Updating pacman repository and installing package helper"
+	sudo pacman -Sy
+	sudo pacman -S --needed --noconfirm pkgbuild-introspection
+
+	echo "-> Download, build and install cower (dependency of pacaur) and pacaur"
+	curl -so /tmp/cower.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz
+	curl -so /tmp/pacaur.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz
+	tar -xzf /tmp/cower.tar.gz -C /tmp
+	tar -xzf /tmp/pacaur.tar.gz -C /tmp
+	gpg --recv-key 1EB2638FF56C0C53
+	cd /tmp/cower; makepkg -sri --noconfirm
+	cd /tmp/pacaur; makepkg -sri --noconfirm
+
+	echo "-> Installing build requirements"
+	pacaur --noconfirm --noedit -S plugn
+
+arch-dokku: arch-setup
+	echo "-> Update package sums, create metadata file and test the build of the package"
+	git describe --tags > /tmp/VERSION
+	cat /tmp/VERSION | cut -d '-' -f 1 | cut -d 'v' -f 2 > /tmp/STABLE_VERSION
+	sed -i -e "s/pkgver=.*/pkgver=`cat /tmp/STABLE_VERSION`/" /dokku-arch/PKGBUILD
+	cd /dokku-arch; updpkgsums; mksrcinfo; makepkg -fd
