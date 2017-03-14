731d012f89dba679d83b2568ea11ec15787c3b04
R3ido101 <oliver_reid_@hotmail.co.uk>
2017-03-14 05:59:16 +0000

    Update bootstrap.sh
    Fixed screaming about installing a key for ubuntu 16.04

--- bootstrap.sh
+++ bootstrap.sh
@@ -125,10 +125,7 @@ install-dokku-from-deb-package() {
   fi
   echo "Installing Docker Key! (If on Ubuntu)"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+  wget -nv -O - https://get.docker.com/ | sh
 
   echo "--> Installing dokku"
   wget -nv -O - https://packagecloud.io/gpg.key | apt-key add -
