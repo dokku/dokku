2851f2c63f67b4e8fedfc7c22e456e693fa9ccc5
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2017-10-05 13:26:20 -0400

    fix: remove duplicate logging functions

--- plugins/common/common.go
+++ plugins/common/common.go
@@ -359,32 +359,3 @@ func PlugnTrigger(triggerName string, args ...string) (string, error) {
 	res, err := sh.Command("plugn", shellArgs...).Output()
 	return string(res), err
 }
-
-// LogInfo1 is the info1 header formatter
-func LogInfo1(text string) {
-	fmt.Fprintln(os.Stdout, fmt.Sprintf("-----> %s", text))
-}
-
-// LogInfo1Quiet is the info1 header formatter (with quiet option)
-func LogInfo1Quiet(text string) {
-	if os.Getenv("DOKKU_QUIET_OUTPUT") != "" {
-		LogInfo1(text)
-	}
-}
-
-// LogInfo2 is the info2 header formatter
-func LogInfo2(text string) {
-	fmt.Fprintln(os.Stdout, fmt.Sprintf("=====> %s", text))
-}
-
-// LogInfo2Quiet is the info2 header formatter (with quiet option)
-func LogInfo2Quiet(text string) {
-	if os.Getenv("DOKKU_QUIET_OUTPUT") != "" {
-		LogInfo2(text)
-	}
-}
-
-// LogWarn is the warning log formatter
-func LogWarn(text string) {
-	fmt.Fprintln(os.Stderr, fmt.Sprintf(" !     %s", text))
-}
