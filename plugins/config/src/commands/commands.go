2fdae593c2f0caa7cd63fbdff7725db83b046e8b
Mary Kate Fain <mkfain1@gmail.com>
2018-03-27 21:29:23 -0400

    Updated help text on config:set to have commands in working order

--- plugins/config/src/commands/commands.go
+++ plugins/config/src/commands/commands.go
@@ -21,7 +21,7 @@ Additional commands:`
 	helpContent = `
     config (<app>|--global), Pretty-print an app or global environment
     config:get (<app>|--global) KEY, Display a global or app-specific config value
-    config:set (<app>|--global) [--encoded] [--no-restart] KEY1=VALUE1 [KEY2=VALUE2 ...], Set one or more config vars
+    config:set [--encoded] [--no-restart] (<app>|--global) KEY1=VALUE1 [KEY2=VALUE2 ...], Set one or more config vars
     config:unset (<app>|--global) KEY1 [KEY2 ...], Unset one or more config vars
     config:export (<app>|--global) [--envfile], Export a global or app environment
     config:keys (<app>|--global) [--merged], Show keys set in environment
