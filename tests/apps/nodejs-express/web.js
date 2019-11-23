906e2c561535bf9d70ca2dc5fc43145c71e39886
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-23 11:51:12 -0500

    feat: remove unnecessary logger from test app
    The logger middleware was moved to the `morgan` package in newer versions of express.

--- tests/apps/nodejs-express/web.js
+++ tests/apps/nodejs-express/web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   response.send('nodejs/express');
