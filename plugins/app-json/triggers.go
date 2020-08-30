617ca885f18754e93e0c957b9919bd9260ccbda0
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2020-08-30 13:37:30 -0400

    fix: run release during predeploy

--- plugins/app-json/triggers.go
+++ plugins/app-json/triggers.go
@@ -4,15 +4,15 @@ func TriggerPostDeploy(appName string, imageTag string) error {
 	if err := executeScript(appName, imageTag, "postdeploy"); err != nil {
 		return err
 	}
-
-	if err := executeScript(appName, imageTag, "release"); err != nil {
-		return err
-	}
 	return nil
 }
 
 func TriggerPreDeploy(appName string, imageTag string) error {
 	if err := executeScript(appName, imageTag, "predeploy"); err != nil {
+		return err
+	}
+
+	if err := executeScript(appName, imageTag, "release"); err != nil {
 		return err
 	}
 	return nil
