480ee7b9aff20696591b08bdff7d3f4757c7c247
Alex Gleason <alex@alexgleason.me>
2022-07-24 11:12:16 -0500

    Update deprecated apt-key command

--- bootstrap.sh
+++ bootstrap.sh
@@ -212,7 +212,7 @@ install-dokku-from-deb-package() {
   fi
 
   echo "--> Installing dokku"
-  wget -nv -O - https://packagecloud.io/dokku/dokku/gpgkey | apt-key add -
+  wget -qO- https://packagecloud.io/dokku/dokku/gpgkey | sudo tee /etc/apt/trusted.gpg.d/dokku.asc
   echo "deb https://packagecloud.io/dokku/dokku/$DOKKU_DISTRO/ $OS_ID main" | tee /etc/apt/sources.list.d/dokku.list
   apt-get update -qq >/dev/null
 
