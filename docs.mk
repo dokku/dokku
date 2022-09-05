f7905b4943c92ce76115d698e838a72c3043cbee
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2022-09-05 14:46:55 -0400

    docs: add code for building docs in ci

---
+++ docs.mk
@@ -0,0 +1,19 @@
+.PHONY: docs-build-image
+docs-build-image:
+	 docker build -f docs/_build/Dockerfile --progress plain -t app/mkdocs .
+
+.PHONY: docs-build
+docs-build:
+	docker run --rm -p 3487:3487 -v "$(PWD):/usr/src/source" --name docs-serve app/mkdocs build
+
+.PHONY: docs-serve
+docs-serve:
+	docker run --rm -p 3487:3487 -v "$(PWD):/usr/src/source" --name docs-serve app/mkdocs serve
+
+.PHONY: docs-deps
+docs-deps:
+	@docker run --rm -p 3487:3487 -v "$(PWD):/usr/src/app" app/mkdocs deps
+
+.PHONY: docs-enter
+docs-enter:
+	docker exec -it docs-serve bash
