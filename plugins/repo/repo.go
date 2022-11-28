94097948f430f3a0d5f94558269391043ba82f4f
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2022-11-28 02:24:27 -0500

    refactor: move herokuish app cache from the filesystem into a docker volume
    This simplifies cache referencing and also paves the way for removing more from the app git directory.
    
    Refs #1558

--- plugins/repo/repo.go
+++ plugins/repo/repo.go
@@ -1,28 +1,20 @@
 package repo
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
 	"github.com/dokku/dokku/plugins/common"
-	"github.com/dokku/dokku/plugins/config"
 )
 
 // PurgeCache deletes the contents of the build cache stored in the repository
 func PurgeCache(appName string) error {
 	cacheDir := strings.Join([]string{common.AppRoot(appName), "cache"}, "/")
-	cacheHostDir := strings.Join([]string{common.AppHostRoot(appName), "cache"}, "/")
-	dokkuGlobalRunArgs := common.MustGetEnv("DOKKU_GLOBAL_RUN_ARGS")
-	image := config.GetWithDefault(appName, "DOKKU_IMAGE", os.Getenv("DOKKU_IMAGE"))
 	if info, _ := os.Stat(cacheDir); info != nil && info.IsDir() {
-		dockerLabelArgs := fmt.Sprintf("--label=com.dokku.app-name=%s", appName)
 		purgeCacheCmd := common.NewShellCmd(strings.Join([]string{
 			common.DockerBin(),
-			"container",
-			"run", "--rm", dockerLabelArgs, dokkuGlobalRunArgs,
-			"-v", strings.Join([]string{cacheHostDir, ":/cache"}, ""), image,
-			`find /cache -depth -mindepth 1 -maxdepth 1 -exec rm -Rf {} ;`}, " "))
+			"volume",
+			"rm", "-f", "cache-$APP"}, " "))
 		purgeCacheCmd.Execute()
 		err := os.MkdirAll(cacheDir, 0644)
 		if err != nil {
