111497f2fdf12637724aa189801c0ab55a564fea
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2023-07-01 04:25:02 -0400

    chore: remove use of inline 'shellcheck disable' calls
    These are already defined in the .shellcheckrc and don't need to be set inline.

--- contrib/dokku-docker-bin.sh
+++ contrib/dokku-docker-bin.sh
@@ -4,5 +4,4 @@ set -eo pipefail
 DOKKU_DOCKER_CONTAINER_NAME=${DOKKU_DOCKER_CONTAINER_NAME:=dokku}
 
 # TODO: handle cases where we need a tty
-# shellcheck disable=SC2086
 docker exec $DOKKU_DOCKER_ENV -i "$DOKKU_DOCKER_CONTAINER_NAME" dokku "$@"
