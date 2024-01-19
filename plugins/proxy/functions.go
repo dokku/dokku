7fbb86093bdbda735d05b1159c970711341e9c77
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-01-19 04:13:55 -0500

    feat: implement global support for setting proxy type

--- plugins/proxy/functions.go
+++ plugins/proxy/functions.go
@@ -5,5 +5,18 @@ import (
 )
 
 func getAppProxyType(appName string) string {
-	return config.GetWithDefault(appName, "DOKKU_APP_PROXY_TYPE", "nginx")
+	return config.GetWithDefault(appName, "DOKKU_APP_PROXY_TYPE", "")
+}
+
+func getComputedProxyType(appName string) string {
+	proxyType := getGlobalProxyType()
+	if proxyType == "" {
+		proxyType = getAppProxyType(appName)
+	}
+
+	return proxyType
+}
+
+func getGlobalProxyType() string {
+	return config.GetWithDefault("--global", "DOKKU_PROXY_TYPE", "nginx")
 }
