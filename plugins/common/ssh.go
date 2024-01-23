ff849cd4f848b7530e42567aead8c80ae92e8a17
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-01-23 04:25:25 -0500

    fix: touch the known hosts file earlier

--- plugins/common/ssh.go
+++ plugins/common/ssh.go
@@ -244,6 +244,10 @@ func (task SshTask) Execute(ctx context.Context) (SshResult, error) {
 		task.Port = 22
 	}
 
+	if err := TouchFile(filepath.Join(os.Getenv("DOKKU_ROOT"), ".ssh", "known_hosts")); err != nil {
+		return SshResult{}, fmt.Errorf("failed to touch known_hosts file: %w", err)
+	}
+
 	auth, err := goph.Key(task.SshKeyPath, "")
 	if err != nil {
 		return SshResult{}, fmt.Errorf("failed to load ssh key: %w", err)
@@ -252,10 +256,6 @@ func (task SshTask) Execute(ctx context.Context) (SshResult, error) {
 	callback, err := goph.DefaultKnownHosts()
 	if err != nil {
 		return SshResult{}, fmt.Errorf("failed to load known hosts: %w", err)
-	}
-
-	if err := TouchFile(filepath.Join(os.Getenv("DOKKU_ROOT"), ".ssh", "known_hosts")); err != nil {
-		return SshResult{}, fmt.Errorf("failed to touch known_hosts file: %w", err)
 	}
 
 	if task.AllowUknownHosts {
