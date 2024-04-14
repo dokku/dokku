244b898fd2a770f27e88b8f2265c6f49e4b54885
Garret Heaton <powdahound@gmail.com>
2024-04-13 20:23:45 -0400

    Remove errant tab character in cron:set help text
    Noticed it was showing up first and with odd indentation in the output of `dokku help --all`. Now it will be indented like the others.

--- plugins/cron/src/commands/commands.go
+++ plugins/cron/src/commands/commands.go
@@ -21,7 +21,7 @@ Additional commands:`
     cron:list <app> [--format json|stdout], List scheduled cron tasks for an app
     cron:report [<app>] [<flag>], Display report about an app
     cron:run <app> <cron_id> [--detach], Run a cron task on the fly
-	cron:set [--global|<app>] <key> <value>, Set or clear a cron property for an app`
+    cron:set [--global|<app>] <key> <value>, Set or clear a cron property for an app`
 )
 
 func main() {
