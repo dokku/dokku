318c21996afdae9e7aef9df59caf3aa75809f1e0
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-11-20 00:59:49 -0500

    fix: use correct variable name

--- plugins/app-json/functions.go
+++ plugins/app-json/functions.go
@@ -148,14 +148,14 @@ func executeScript(appName string, image string, imageTag string, phase string) 
 	}
 
 	if command == "" {
-		common.LogVerbose(fmt.Sprintf("No %s task found, skipping", phaseName))
+		common.LogVerbose(fmt.Sprintf("No %s task found, skipping", phase))
 		return nil
 	}
 
 	if phase == "predeploy" {
-		common.LogVerbose(fmt.Sprintf("Executing %s task from %s: %s", phaseName, phaseSource, command))
+		common.LogVerbose(fmt.Sprintf("Executing %s task from %s: %s", phase, phaseSource, command))
 	} else {
-		common.LogVerbose(fmt.Sprintf("Executing %s task from %s in ephemeral container: %s", phaseName, phaseSource, command))
+		common.LogVerbose(fmt.Sprintf("Executing %s task from %s in ephemeral container: %s", phase, phaseSource, command))
 	}
 
 	isHerokuishImage := common.IsImageHerokuishBased(image, appName)
@@ -267,15 +267,15 @@ func executeScript(appName string, image string, imageTag string, phase string) 
 	defer cleanupDeploymentContainer(containerID, phase)
 
 	if !waitForExecution(containerID) {
-		common.LogInfo2Quiet(fmt.Sprintf("Start of %s %s task (%s) output", appName, phaseName, containerID[0:9]))
+		common.LogInfo2Quiet(fmt.Sprintf("Start of %s %s task (%s) output", appName, phase, containerID[0:9]))
 		common.LogVerboseQuietContainerLogs(containerID)
-		common.LogInfo2Quiet(fmt.Sprintf("End of %s %s task (%s) output", appName, phaseName, containerID[0:9]))
-		return fmt.Errorf("Execution of %s task failed: %s", phaseName, command)
+		common.LogInfo2Quiet(fmt.Sprintf("End of %s %s task (%s) output", appName, phase, containerID[0:9]))
+		return fmt.Errorf("Execution of %s task failed: %s", phase, command)
 	}
 
-	common.LogInfo2Quiet(fmt.Sprintf("Start of %s %s task (%s) output", appName, phaseName, containerID[0:9]))
+	common.LogInfo2Quiet(fmt.Sprintf("Start of %s %s task (%s) output", appName, phase, containerID[0:9]))
 	common.LogVerboseQuietContainerLogs(containerID)
-	common.LogInfo2Quiet(fmt.Sprintf("End of %s %s task (%s) output", appName, phaseName, containerID[0:9]))
+	common.LogInfo2Quiet(fmt.Sprintf("End of %s %s task (%s) output", appName, phase, containerID[0:9]))
 
 	if phase != "predeploy" {
 		return nil
